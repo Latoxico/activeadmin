db7383c4b79970c48e4331db012d3cc22ed2eac0
latoxico <ricsjp@gmail.com>
2015-11-19 22:12:11 +0000

    segundo teste

--- activeadmin/app/admin/book.rb
+++ activeadmin/app/admin/book.rb
@@ -7,8 +7,15 @@ ActiveAdmin.register Book do
 #
 # or
 #
- permit_params :name ,:author ,:description
+ permit_params :name ,:author ,:description,:photo
  
-
-
+    form do |f|
+            f.inputs "Books" do 
+                f.inputs :name 
+                f.inputs :author
+                f.inputs :description
+                f.inputs :photo, :as => :file
+            end
+            f.actions
+    end
 end
